05bce9e7917c67b86d5a218c2ade17e23dea0558
tnuiriankh <tnuiriankh@gmail.com>
2022-10-17 14:33:17 -0400

    Overhaul playRound function

--- rps.js
+++ rps.js
@@ -16,39 +16,26 @@ console.log(getComputerChoice());
 
 //Player chooses to complete inputs
 
-let playerSelection = prompt("Pick one" , "Rock, paper, or scissors");
-
+let playerSelection = prompt("Pick one" , "Rock, paper, or scissors?");
+let result;
 //Game is played out
 
-function playRound(playerSelection, computerPick) {
-    let result;
+function playRound(playerSelection, getComputerChoice) {
 
-    if (playerSelection === "rock", computerPick === "Scissors") {
-        result = "You win!"
-    } else if (playerSelection === "rock", computerPick === "Rock") {
-        result = "It's a tie!"
-    } else if (playerSelection === "rock", computerPick === "Paper") {
-        result = "You lose!"
+    if (playerSelection == getComputerChoice) {
+       result = "It's a tie!";
+    } else if (
+        (getComputerChoice == "Rock" && playerSelection == "scissors") ||
+        (getComputerChoice == "Paper" && playerSelection == "rock") ||
+        (getComputerChoice == "Scissors" && playerSelection == "paper") 
+        ) {
+        result = "You lose";
+    } else if (
+        (playerSelection == "scissors" && getComputerChoice == "Paper") ||
+        (playerSelection == "rock" && getComputerChoice == "Scissors") ||
+        (playerSelection == "paper" && getComputerChoice == "rock")
+    ) {
+        result = "You win!";
     }
-
-    if (playerSelection === "paper", computerPick === "Scissors") {
-        result = "You lose!"
-    } else if (playerSelection === "paper", computerPick === "Rock") {
-        result = "You win!"
-    } else if (playerSelection === "paper", computerPick === "Paper") {
-        result = "It's a tie!"
-    }
-
-    if (playerSelection === "scissors", computerPick === "Scissors") {
-        result = "It's a tie!"
-    } else if (playerSelection === "scissors", computerPick === "Rock") {
-        result = "You lose!"
-    } else if (playerSelection === "scissors", computerPick === "Paper") {
-        result = "You win!"
-    }
-
-    return(result);
-
-    }
-
-console.log(playRound());
+        
+}
